4fa0bd757b0bcec9b1a90996affb810225cd549a
Đặng Quang Linh <linhlay2809@gmail.com>
2021-11-03 17:17:52 +0700

    Update date (11/03/2021)
    Add CameraFollow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float posY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, player.transform.position.y + posY, transform.position.z); 
+    }
+}
